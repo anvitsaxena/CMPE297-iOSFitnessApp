b372df4ec1777656b8f3902fe52d6428b13ae36e
Anvit Saxena <anvit.saxena@sjsu.edu>
2018-01-12 22:20:06 -0800

    Moved iOS Fitness App Project from CMPE297-iOS-MapKit repository

---
+++ coremotion/AppDelegate.swift
@@ -0,0 +1,38 @@
+//
+//  AppDelegate.swift
+//  stepcount
+//
+//  Created by Anvit on 10/14/17.
+//  Copyright © 2017 Anvit. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+    }
+
+
+}
+
---
+++ coremotionTests/coremotionTests.swift
@@ -0,0 +1,23 @@
+
+import XCTest
+@testable import stepcount
+
+class coremotionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExample() {
+    }
+    
+    func testPerformanceExample() {
+        self.measure {
+        }
+    }
+    
+}
